398e72b854b1d5a75b0f5072dc6a04bc25fe49fe
anphan25 <73126703+anphan25@users.noreply.github.com>
2021-11-12 14:35:37 +0700

    init

---
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,36 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	//need to inject the session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomer() {
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		//create query
+		Query<Customer> theQuery = currentSession.createQuery("from Customer",Customer.class);
+		//get result list
+		List<Customer> customers = theQuery.getResultList();
+		// return the result
+		
+		return customers;
+	}
+
+}
